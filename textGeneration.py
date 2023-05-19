5bcbc167c33f1a0b1875112b97b7e454feb412f1
Victor <vitcheffvi@cy-tech.fr>
2023-05-19 11:53:38 +0200

    [textGenration.py] add text examples

--- textGeneration.py
+++ textGeneration.py
@@ -10,11 +10,17 @@ def predict(x):
 
 
 with gr.Blocks() as demo:
-    gr.Markdown("Generate some text with this demo ! ")
+    gr.Markdown("# Generate some text with this demo ! ")
     input = gr.Textbox(label="Input Text")
     output = gr.Textbox(label="Output Text")
     generate_btn = gr.Button("Generate")
     generate_btn.click(fn=predict, inputs=input, outputs=output)
+    gr.Markdown("## Examples")
+    gr.Examples(examples=["My name is jean and i like", "I go every day at the "],
+                cache_examples=True,
+                inputs=input,
+                outputs=output,
+                fn=predict)
 
 
 demo.launch(server_name="0.0.0.0")
