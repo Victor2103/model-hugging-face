8dd389c482248ab7f21d2c2cd22fde05d4199743
VictorOvh <victor.notebook@ovh.com>
2023-05-22 09:35:08 +0000

    [yoloTiny.py] add gradio to the model

--- yoloTiny.py
+++ yoloTiny.py
@@ -4,28 +4,43 @@ import torch
 import requests
 import gradio as gr
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+# image = Image.open(requests.get(url, stream=True).raw)
 
 model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
 image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
 
-inputs = image_processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
 
-# model predicts bounding boxes and corresponding COCO classes
-logits = outputs.logits
-bboxes = outputs.pred_boxes
+def predict(x):
+    inputs = image_processor(images=x, return_tensors="pt")
+    outputs = model(**inputs)
+    # model predicts bounding boxes and corresponding COCO classes
+    logits = outputs.logits
+    bboxes = outputs.pred_boxes
+
+    # print results
+    target_sizes = torch.tensor([x.size[::-1]])
+    results = image_processor.post_process_object_detection(
+        outputs, threshold=0.9, target_sizes=target_sizes)[0]
+    returning = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        returning += f"Detected {model.config.id2label[label.item()]} with confidence {round(score.item(), 3)} at location {box} \n"
+    return (returning)
 
 
-# print results
-target_sizes = torch.tensor([image.size[::-1]])
-results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
-        f"Detected {model.config.id2label[label.item()]} with confidence "
-        f"{round(score.item(), 3)} at location {box}"
-    )
+with gr.Blocks() as demo:
+    gr.Markdown("# Detect object from a given image ! ")
+    image = gr.Image(label="Input Image", type="pil")
+    output = gr.Textbox(label="Output Text")
+    generate_btn = gr.Button("Generate")
+    generate_btn.click(fn=predict, inputs=image, outputs=output)
+    gr.Markdown("## Examples")
+    """gr.Examples(examples=["My name is James and i like", "I go every day at the "],
+                cache_examples=True,
+                inputs=input,
+                outputs=output,
+                fn=predict)"""
 
 
+demo.launch(server_name="0.0.0.0")
