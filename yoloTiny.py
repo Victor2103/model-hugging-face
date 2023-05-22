7ae841fa02606e40786973401d75499977d3585e
VictorOvh <victor.notebook@ovh.com>
2023-05-22 15:50:47 +0000

    [yoloTiny.py] add function pipeline transformers

--- yoloTiny.py
+++ yoloTiny.py
@@ -1,8 +1,6 @@
 from transformers import YolosImageProcessor, YolosForObjectDetection, pipeline
 import torch
 import gradio as gr
-import requests
-from PIL import Image
 
 # url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 # image = Image.open(requests.get(url, stream=True).raw)
@@ -11,25 +9,13 @@ model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
 image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
 
 
-def test(x):
-    obj_detector=pipeline("object-detection",model=model,tokenizer=image_processor)
-    return(obj_detector(x))
-
 def predict(x):
-    inputs = image_processor(images=x, return_tensors="pt")
-    outputs = model(**inputs)
+    obj_detector=pipeline("object-detection",model=model,feature_extractor=image_processor)
+    return(str(obj_detector(x)))
 
-    # print results
-    target_sizes = torch.tensor([x.size[::-1]])
-    results = image_processor.post_process_object_detection(
-        outputs, threshold=0.9, target_sizes=target_sizes)[0]
-    returning = ""
-    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-        box = [round(i, 2) for i in box.tolist()]
-        returning += f"Detected {model.config.id2label[label.item()]} with confidence {round(score.item(), 3)} at location {box} \n"
-    return (returning)
 
-"""
+
+
 with gr.Blocks() as demo:
     gr.Markdown("# Detect object from a given image ! ")
     image = gr.Image(label="Input Image", type="pil")
@@ -45,9 +31,9 @@ with gr.Blocks() as demo:
 
 
 demo.launch(server_name="0.0.0.0")
+
 """
-
-
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 print(test(image))
+"""
