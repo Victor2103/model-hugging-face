062eb4c58c885e83cb80de9047479af6c19669e7
Victor <notebook@ovh.com>
2023-05-23 13:22:16 +0000

    [yoloTiny.py] add the image and text output

--- yoloTiny.py
+++ yoloTiny.py
@@ -1,39 +1,35 @@
 from transformers import YolosImageProcessor, YolosForObjectDetection, pipeline
 import gradio as gr
-import requests
-from PIL import Image, ImageDraw
+#import requests
+from PIL import ImageDraw
 import torch
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+#image = Image.open(requests.get(url, stream=True).raw)
 
 model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
 image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
 
 
-def predict(x):
+def predict(im):
     obj_detector = pipeline("object-detection", model=model,
                             feature_extractor=image_processor)
-    return (str(obj_detector(x)), x)
-
-
-with torch.no_grad():
-    inputs = image_processor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    target_sizes = torch.tensor([image.size[::-1]])
-    results = image_processor.post_process_object_detection(
-        outputs, threshold=0.5, target_sizes=target_sizes)[0]
-
-
-draw = ImageDraw.Draw(image)
-
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    x, y, x2, y2 = tuple(box)
-    draw.rectangle((x, y, x2, y2), outline="red", width=1)
-    draw.text((x, y), model.config.id2label[label.item()], fill="white")
-
-# image.save("test.png")
+    with torch.no_grad():
+        inputs = image_processor(images=im, return_tensors="pt")
+        outputs = model(**inputs)
+        target_sizes = torch.tensor([im.size[::-1]])
+        results = image_processor.post_process_object_detection(
+            outputs, threshold=0.5, target_sizes=target_sizes)[0]
+    draw = ImageDraw.Draw(im)
+    text=""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        x, y, x2, y2 = tuple(box)
+        draw.rectangle((x, y, x2, y2), outline="red", width=1)
+        draw.text((x, y), model.config.id2label[label.item()], fill="white")
+        text+=f"Detected {model.config.id2label[label.item()]} with confidence "
+        text+=f"{round(score.item(), 3)} at location {box} \n"
+    return ([text,im])
 
 
 with gr.Blocks() as demo:
@@ -42,8 +38,7 @@ with gr.Blocks() as demo:
     output_text = gr.Textbox(label="Output Text")
     output_image = gr.Image(label="Output Image", type="pil")
     generate_btn = gr.Button("Generate")
-    generate_btn.click(fn=predict, inputs=image, outputs=[
-                       output_text, output_image])
+    generate_btn.click(fn=predict, inputs=image, outputs=[output_text,output_image])
     gr.Markdown("## Examples")
     gr.Examples(examples=["example.jpg"],
                 cache_examples=True,
