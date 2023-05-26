f2be2526c93981e535661a2064f5c07408186d83
Victor <vitcheffvi@cy-tech.fr>
2023-05-26 16:20:19 +0200

    [test.py] do some small tests

--- test.py
+++ test.py
@@ -1,41 +1,13 @@
+import json
 import requests
-from gradio import processing_utils
+headers = {"Authorization": f"Bearer "}
+API_URL = "https://api-inference.huggingface.co/models/hustvl/yolos-tiny"
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    print(response)
+    return json.loads(response.content.decode("utf-8"))
+data = query("examples/example_1.jpg")
 
-with open("examples/example_1.jpg", "rb") as image_file:
-    encoded_string = processing_utils.encode_url_or_file_to_base64(image_file)
-
-headers={'Accept': 'application/json'}
-
-body={
-    "fn_index": 0,
-    "data": [
-        f"data:image/jpeg;base64,{encoded_string}"
-    ],
-    "event_data": None,
-    "session_hash": "2zx2slhv3qm"
-}
-
-res=requests.post("https://7f3b114c-120b-42ac-88e4-84982e9e4bc0.app.gra.ai.cloud.ovh.net/run/predict",json=body,headers=headers)
-
-print(body)
-print(res.text)
-
-
-
-
-
-
-
-
-
-
-"""
-headers={'Accept': 'application/json',
-         'Authorization': f'Bearer SAnNu2i6R+K6JOYaAUDclnTWx1XH3Ck7+hIfr2dWj4oLEbt+XvhMnvUUegm0QFY9'}
-
-body={"fn_index":0,"data":["can you"],"event_data":None,"session_hash":"q75l89if3v8"}
-
-res=requests.post("https://f29f35cd-dce1-4a16-8cbc-c1f548cc7a46.app.gra.ai.cloud.ovh.net/run/predict",json=body,headers=headers)
-
-print(res.text)
-"""
+print(data)
