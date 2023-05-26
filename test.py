336ff963abd72f1e6e323b63002c7357ae326961
Victor <vitcheffvi@cy-tech.fr>
2023-05-26 18:48:38 +0200

    [test.py] format document

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
+from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, pipeline
 from fastapi import FastAPI
 
 app = FastAPI()
 
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, pipeline
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = TFGPT2LMHeadModel.from_pretrained('gpt2')
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/gpt2/{input_text}")
 async def read_item(input_text: str):
