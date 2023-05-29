bce0660dfcff958c6e5c68b2a5a68af63082cc40
Victor <notebook@ovh.com>
2023-05-29 09:20:41 +0000

    [yoloTiny.py] add the choices of several models

--- yoloTiny.py
+++ yoloTiny.py
@@ -1,4 +1,4 @@
-from transformers import YolosImageProcessor, YolosForObjectDetection, pipeline
+from transformers import YolosImageProcessor, YolosForObjectDetection,DetrImageProcessor, DetrForObjectDetection, pipeline
 import gradio as gr
 # import requests
 from PIL import ImageDraw
@@ -7,11 +7,18 @@ import torch
 # url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 # image = Image.open(requests.get(url, stream=True).raw)
 
-model = YolosForObjectDetection.from_pretrained('hustvl/yolos-small')
-image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-small")
+
+models=['hustvl/yolos-small','hustvl/yolos-tiny','facebook/detr-resnet-50']
 
 
-def predict(im):
+
+def predict(im,option_model):
+    if option_model == 'facebook/detr-resnet-50':
+        image_processor = DetrImageProcessor.from_pretrained(option_model)
+        model = DetrForObjectDetection.from_pretrained(option_model)
+    else:
+        model = YolosForObjectDetection.from_pretrained(option_model)
+        image_processor = YolosImageProcessor.from_pretrained(option_model)
     with torch.no_grad():
         inputs = image_processor(images=im, return_tensors="pt")
         outputs = model(**inputs)
@@ -34,12 +41,14 @@ title = "Object Detection"
 
 with gr.Blocks(title="Object-detection") as demo:
     gr.Markdown("# Detect object from a given image ! ")
+    with gr.Column():
+        options = gr.Dropdown(choices=models,label='Select Object Detection Model',show_label=True)
     with gr.Row():
         image = gr.Image(label="Input Image", type="pil")
         output_text = gr.Textbox(label="Output Text")
         output_image = gr.Image(label="Output Image", type="pil")
     generate_btn = gr.Button("Generate")
-    generate_btn.click(fn=predict, inputs=image, outputs=[
+    generate_btn.click(fn=predict, inputs=[image,options], outputs=[
                        output_text, output_image])
     gr.Markdown("## Examples")
     gr.Examples(examples=["examples/example_1.jpg", "examples/example_2.jpg", "examples/example_3.jpg", "examples/example_4.jpg"],
@@ -47,6 +56,5 @@ with gr.Blocks(title="Object-detection") as demo:
                 inputs=image,
                 outputs=[output_text, output_image],
                 fn=predict)
-
 
 demo.launch(server_name="0.0.0.0")
