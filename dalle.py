33c5af301badab15c6a3dba6ed68a740da0d7709
Victor <notebook@ovh.com>
2023-06-05 09:55:18 +0000

    [dalle.py] add image generation with text app

---
+++ dalle.py
@@ -0,0 +1,14 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+model_id = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to(device)
+
+prompt = "an astronaut with an elephant on Mars"
+image = pipe(prompt).images[0]  
+    
+image.save("astronaut_and_elephant.png")
