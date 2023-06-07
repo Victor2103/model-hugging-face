9d06df260044b21667ce553531819b91468af27e
Victor <vitcheffvi@cy-tech.fr>
2023-06-07 15:58:42 +0200

    [app.py] create the gradio app

--- chatbot-LLM/app.py
+++ chatbot-LLM/app.py
@@ -1,29 +1,53 @@
 from transformers import AutoTokenizer
 import transformers
 import torch
+import gradio as gr
+from fastapi import FastAPI
+
+
+app = FastAPI()
 
 model = "tiiuae/falcon-7b-instruct"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    framework="pt",
-    device_map="auto"
-)
-sequences = pipeline(
-   "Oui Oui is a cartoon caracter. He has a taxi and can honk when he wants. He has a taxi race. Imagine the discussion with this caracter in the taxi. He is very special and don't like to discuss.",
-    max_length=1000,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
-    
+
+def predict(prompt):
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        framework="pt",
+        device_map="auto"
+    )
+    sequences = pipeline(
+        prompt,
+        max_length=1000,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    result = ""
+    for seq in sequences:
+        result += f"Result: {seq['generated_text']}"
+    return (result)
 
 
+with gr.Blocks(title="Chat GPT") as demo:
+    gr.Markdown("# Speak with a chatbot here !")
+    text_input = gr.Textbox(label="Input Text")
+    output = gr.Textbox(label="Output Text")
+    generate_btn = gr.Button("Generate")
+    generate_btn.click(fn=predict, inputs=[
+                       text_input], outputs=output)
+    gr.Markdown("## Examples")
+    gr.Examples(examples=[["Imagine the journey of a simple man living in London who go to work. He doesn't have a car and childrens."],
+                          ["I want to write a story to a little child of 10 years. Help me do this"]],
+                cache_examples=True,
+                inputs=[text_input],
+                outputs=output,
+                fn=predict)
+
+app = gr.mount_gradio_app(app, demo, path='/')
